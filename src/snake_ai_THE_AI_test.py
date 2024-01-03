55d00720c298cf2e3259b93d15efa9021a7467c8
Peter <p.vesterenglarsen@gmail.com>
2024-01-03 12:52:08 +0100

    startkode virker

---
+++ src/snake_ai_THE_AI_test.py
@@ -0,0 +1,96 @@
+#AI VERSION
+import snake_ai.src.snake_ai_test as snake_ai_test
+import pygame
+
+env = snake_ai_test
+exit_program = False
+render = True
+
+while not exit_program:
+	# handling key events
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				env.change_to = 'UP'
+			if event.key == pygame.K_DOWN:
+				env.change_to = 'DOWN'
+			if event.key == pygame.K_LEFT:
+				env.change_to = 'LEFT'
+			if event.key == pygame.K_RIGHT:
+				env.change_to = 'RIGHT'
+			if event.key == pygame.K_r:
+				env.snake_position, env.fruit_position,env.fruit_spawn,env.score,env.direction,env.change_to, env.snake_body = env.reset() ########
+	if event.type == pygame.QUIT:
+		exit_program = True
+		#env.game_over()
+		pygame.quit()
+		env.close()
+		break
+
+	# If two keys pressed simultaneously
+	# we don't want snake to move into two 
+	# directions simultaneously
+	if env.change_to == 'UP' and env.direction != 'DOWN':
+		env.direction = 'UP'
+	if env.change_to == 'DOWN' and env.direction != 'UP':
+		env.direction = 'DOWN'
+	if env.change_to == 'LEFT' and env.direction != 'RIGHT':
+		env.direction = 'LEFT'
+	if env.change_to == 'RIGHT' and env.direction != 'LEFT':
+		env.direction = 'RIGHT'
+
+	# Moving the snake
+	if env.direction == 'UP':
+		env.snake_position[1] -= 10
+	if env.direction == 'DOWN':
+		env.snake_position[1] += 10
+	if env.direction == 'LEFT':
+		env.snake_position[0] -= 10
+	if env.direction == 'RIGHT':
+		env.snake_position[0] += 10
+
+	# Snake body growing mechanism
+	# if fruits and snakes collide then scores
+	# will be incremented by 10
+	env.snake_body.insert(0, list(env.snake_position))
+	if env.snake_position[0] == env.fruit_position[0] and env.snake_position[1] == env.fruit_position[1]:
+		env.score += 10
+		env.fruit_spawn = False
+	else:
+		env.snake_body.pop()
+	
+	
+	if not env.fruit_spawn:
+		env.fruit_position = [env.random.randrange(1, (env.window_x//10)) * 10, 
+						env.random.randrange(1, (env.window_y//10)) * 10]
+		
+		
+	env.fruit_spawn = True
+	env.game_window.fill(env.black)
+	
+	for pos in env.snake_body:
+		pygame.draw.rect(env.game_window, env.green,
+						pygame.Rect(pos[0], pos[1], 10, 10))
+	pygame.draw.rect(env.game_window, env.white, pygame.Rect(
+		env.fruit_position[0], env.fruit_position[1], 10, 10))
+
+	# Game Over conditions
+	if env.snake_position[0] < 0 or env.snake_position[0] > env.window_x-10:
+		env.game_over()
+		break
+	if env.snake_position[1] < 0 or env.snake_position[1] > env.window_y-10:
+		env.game_over()
+		break
+
+	# Touching the snake body
+	for block in env.snake_body[1:]:
+		if env.snake_position[0] == block[0] and env.snake_position[1] == block[1]:
+			env.game_over()
+			break
+	if render:
+		#   displaying score continuously
+		env.show_score(1, env.white, 'times new roman', 20)
+		# Refresh game screen
+		pygame.display.update()
+		# Frame Per Second /Refresh Rate
+		env.fps.tick(env.snake_speed)
---
+++ src/snake_ai_test.py
@@ -0,0 +1,114 @@
+# importing libraries
+import pygame
+import time
+import random
+
+snake_speed = 15
+
+# Window size
+window_x = 720
+window_y = 480
+
+# defining colors
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+# Initialising pygame
+pygame.init()
+
+# Initialise game window
+pygame.display.set_caption('GeeksforGeeks Snakes')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
+
+# defining snake default position
+snake_position = [100, 50]
+
+# defining first 4 blocks of snake body
+snake_body = [[100, 50],
+			[90, 50],
+			[80, 50],
+			[70, 50]
+			]
+# fruit position
+fruit_position = [random.randrange(1, (window_x//10)) * 10, 
+				random.randrange(1, (window_y//10)) * 10]
+
+fruit_spawn = True
+
+# setting default snake direction towards
+# right
+direction = 'RIGHT'
+change_to = direction
+
+# initial score
+score = 0
+
+# displaying Score function
+def show_score(choice, color, font, size):
+
+	# creating font object score_font
+	score_font = pygame.font.SysFont(font, size)
+	
+	# create the display surface object 
+	# score_surface
+	score_surface = score_font.render('Score : ' + str(score), True, color)
+	
+	# create a rectangular object for the text
+	# surface object
+	score_rect = score_surface.get_rect()
+	
+	# displaying text
+	game_window.blit(score_surface, score_rect)
+
+# game over function
+def game_over():
+
+	# creating font object my_font
+	my_font = pygame.font.SysFont('times new roman', 50)
+	
+	# creating a text surface on which text 
+	# will be drawn
+	game_over_surface = my_font.render(
+		'Your Score is : ' + str(score), True, red)
+	
+	# create a rectangular object for the text 
+	# surface object
+	game_over_rect = game_over_surface.get_rect()
+	
+	# setting position of the text
+	game_over_rect.midtop = (window_x/2, window_y/4)
+	
+	# blit will draw the text on screen
+	game_window.blit(game_over_surface, game_over_rect)
+	pygame.display.flip()
+	
+	# after 2 seconds we will quit the program
+	#time.sleep(2)
+	
+	# deactivating pygame library
+	pygame.quit()
+	
+	# quit the program
+	quit()
+def close():
+	pygame.quit()
+def reset():
+	start_pos = [100,50]
+	start_fruit = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
+	fruit_spawn_local = True
+	score_local = 0
+	direction_local = 'RIGHT'
+	change_to_local = direction_local
+	snake_body_local = [[100, 50],
+			[90, 50],
+			[80, 50],
+			[70, 50]
+			]
+	return start_pos, start_fruit, fruit_spawn_local, score_local, direction_local, change_to_local, snake_body_local
+	
