ed505762da5969b429569b580664ec19877b26b8
John Doe <nicholas.borch@yahoo.com>
2024-01-04 15:49:05 +0100

    oprydning Game Over funktion fjernet

--- src/snake_ai_test.py
+++ src/snake_ai_test.py
@@ -45,8 +45,6 @@ def spawn_apple(snake_coordinates):
 		return "WINNER"
 
 
-
-
 # displaying Score function
 def show_score(choice, color, font, size):
 
@@ -64,36 +62,6 @@ def show_score(choice, color, font, size):
 	# displaying text
 	game_window.blit(score_surface, score_rect)
 
-# game over function
-def game_over():
-
-	# creating font object my_font
-	my_font = pygame.font.SysFont('times new roman', 50)
-	
-	# creating a text surface on which text 
-	# will be drawn
-	game_over_surface = my_font.render(
-		'Your Score is : ' + str(score), True, red)
-	
-	# create a rectangular object for the text 
-	# surface object
-	game_over_rect = game_over_surface.get_rect()
-	
-	# setting position of the text
-	game_over_rect.midtop = (window_x/2, window_y/4)
-	
-	# blit will draw the text on screen
-	game_window.blit(game_over_surface, game_over_rect)
-	pygame.display.flip()
-	
-	# after 2 seconds we will quit the program
-	#time.sleep(2)
-	
-	# deactivating pygame library
-	pygame.quit()
-	
-	# quit the program
-	quit()
 
 def close():
 	pygame.quit()
