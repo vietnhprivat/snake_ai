867f0f9b8aa6b1241c4e4534b89d04c92534adea
Peter <p.vesterenglarsen@gmail.com>
2024-01-09 18:12:49 +0100

    model saving improved

--- src/Classes/model_dql.py
+++ src/Classes/model_dql.py
@@ -17,13 +17,13 @@ class Linear_QNet(nn.Module):
         x = self.linear2(x)
         return x
 
-    def save(self, file_name='model.pth'):
+    def save(self, file_name='model.pth', index=0):
         model_folder_path = '.\DQL_models./model'
         if not os.path.exists(model_folder_path):
             os.makedirs(model_folder_path)
 
         file_name = os.path.join(model_folder_path, file_name)
-        torch.save(self.state_dict(), file_name)
+        torch.save(self.state_dict(), f"{file_name}{index}")
 
 
 class QTrainer:
