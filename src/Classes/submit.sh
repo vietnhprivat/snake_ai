84f8c89541a95ba6985c932f2efc22db452d4af2
Viet Nguyen <vietnhprivat@gmail.com>
2024-01-11 17:12:20 +0100

    klar til at løbe tab q i hpc

--- src/Classes/submit.sh
+++ src/Classes/submit.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#BSUB -J jobnam
-#BSUB -o jobname%J.out
-#BSUB -e jobname%J.err
+#BSUB -J qlearn_optim
+#BSUB -o qlearn_done%J.out
+#BSUB -e error_ql%J.err
 #BSUB -n 4
 #BSUB -q gpua10
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -R "rusage[mem=8G]"
+#BSUB -R "rusage[mem=4G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 8:00
+#BSUB -W 23:59
 ### -- send notification at start --
 #BSUB -B
 ### -- send notification at completion--
@@ -20,4 +20,4 @@
 cd /zhome/db/e/206305/snake_ai
 source intelsys/bin/activate
 cd /zhome/db/e/206305/snake_ai/src/Classes
-python agent.py
+python model_optimizer.py
