d56f3313725b4d2a380f65e3d30c9d13b852f803
Viet Nguyen <vietnhprivat@gmail.com>
2024-01-14 14:39:57 +0100

    ændringer til submit fil til hpc

--- src/Classes/submit.sh
+++ src/Classes/submit.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#BSUB -J qlearn_optim
-#BSUB -o qlearn_done%J.out
-#BSUB -e qlearn_error_ql%J.err
-#BSUB -n 4
-#BSUB -q gpua10
+#BSUB -J onestep
+#BSUB -o onestep_output%J.out
+#BSUB -e onestep_error_ql%J.err
+#BSUB -n 24
+#BSUB -q gpuqim
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -R "rusage[mem=4G]"
+#BSUB -R "rusage[mem=12G]"
 #BSUB -R "span[hosts=1]"
 #BSUB -W 23:59
 ### -- send notification at start --
@@ -14,10 +14,11 @@
 #BSUB -N
 # end of BSUB options
 
-# module load cuda/11.8
-
+nvidia-smi
+# Load the cuda module
+module load cuda/11.6
 
 cd /zhome/db/e/206305/snake_ai
 source intelsys/bin/activate
 cd /zhome/db/e/206305/snake_ai/src/Classes
-python model_optimizer.py
+python agent_onestep.py
